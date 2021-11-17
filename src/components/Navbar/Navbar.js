f5e030e60979604568e3f57f63a217805a260a60
Tevvin Strong <tevvindstrong@gmail.com>
2021-11-16 23:39:13 -0500

    Created components folder along with navbar component and jumbotron component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Navbar.css';
+
+const NavBar = () => {
+    return (
+        <>
+        <div className="navbar-container">
+            <div className="nav-bar">
+            <a href="/">
+                TRVL <i class="fab fa-typo3" aria-hidden="true"></i>
+            </a>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default NavBar;
