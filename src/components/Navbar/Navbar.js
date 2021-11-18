822c8d52fc920429fdc750be8badbce911aac77e
Tevvin Strong <tevvindstrong@gmail.com>
2021-11-17 22:47:52 -0500

    Completed navbar structur and styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
 import './Navbar.css';
-
+// TODO: Tomorrow begin working on navbar functionality
 const NavBar = () => {
     return (
         <>
         <div className="navbar-container">
             <div className="nav-bar">
-            <a href="/">
-                TRVL <i class="fab fa-typo3" aria-hidden="true"></i>
-            </a>
+                <div className="left-side">
+                    <a href="/" className="company-name">
+                        TRVL <i class="fab fa-typo3" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div className="right-side">
+                    <a href="/">Home</a>
+                    <a href="/">Services</a>
+                    <a href="/">Products</a>
+                    <a href="/" className="signup-text">SIGN UP</a>
+                </div>
             </div>
         </div>
         </>
