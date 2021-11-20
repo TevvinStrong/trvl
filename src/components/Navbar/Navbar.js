8791982c91b37502b86b59ae0ea90e7121a20415
Tevvin Strong <tevvindstrong@gmail.com>
2021-11-19 22:59:16 -0500

    Solved Router error preventing links to function properly

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-import App from '../../App';
+// import App from '../../App';
+// import Services from '../Navbar/ServicesComponent/Services';
 
 import './Navbar.css';
-// TODO: Tomorrow begin working on navbar functionality
+
 const NavBar = () => {
     return (
         <>
@@ -16,13 +17,12 @@ const NavBar = () => {
                     </a>
                 </div>
                 <div className="right-side">
-                    <Route>
-                        <Link to="/home">Home</Link>
-                        <Link to="/services">Services</Link>
-                        <Link to="/products">Products</Link>
-                        <Link to="/signup" className="signup-text">SIGN UP</Link>
-                        {/* <a href="/" className="signup-text">SIGN UP</a> */}
-                    </Route>
+                    <nav>
+                        <NavLink to="/home">Home</NavLink>
+                        <NavLink to="/services">Services</NavLink>
+                        <NavLink to="/products">Products</NavLink>
+                        <NavLink to="/signup" className="signup-text">SIGN UP</NavLink>
+                    </nav>
                 </div>
             </div>
         </div>
