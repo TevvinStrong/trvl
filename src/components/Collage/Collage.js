004c77fa9dc426387ba48c7624e6017d4fef652f
Tevvin Strong <tevvindstrong@gmail.com>
2021-12-02 21:35:26 -0500

    Created collage folder with collagejs and css file

---
+++ src/components/Collage/Collage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Collage.css';
+
+const Collage = () => {
+    return (
+        <>
+            <div className="collage">
+                <div className="collage-content">
+                
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Collage;
