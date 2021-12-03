e08d1e715f530c436b304befa940272931c75230
Tevvin Strong <tevvindstrong@gmail.com>
2021-12-02 21:58:55 -0500

    Impored images over collage js file

--- src/components/Collage/Collage.js
+++ src/components/Collage/Collage.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import MountainValley from '../../Images/Collage/MountainValleyPexels-photo-1853371.jpeg';
+import Surfers from '../../Images/Collage/SurfersPexels-photo-4321827.jpeg';
+import MountainRange from '../../Images/Collage/MountainValleyPexels-photo-1853371.jpeg';
+import Island from '../../Images/Collage/IslandPexels-photo-931018.jpeg';
+
 import './Collage.css';
 
 const Collage = () => {
@@ -7,7 +12,10 @@ const Collage = () => {
         <>
             <div className="collage">
                 <div className="collage-content">
-                
+                    <img src={MountainValley} alt="Nountain Valley" />
+                    <img src={Surfers} alt="Surfers Surfing in Hawaii" />
+                    <img src={MountainRange} alt="Mountain Range" />
+                    <img src={Island} alt="Island Beach" />
                 </div>
             </div>
         </>
