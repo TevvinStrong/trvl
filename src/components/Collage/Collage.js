8efd412327d500d3459eb22c2e7a8dee0e8a0015
Tevvin Strong <tevvindstrong@gmail.com>
2022-06-01 23:31:06 -0400

    Loading images in as state

--- src/components/Collage/Collage.js
+++ src/components/Collage/Collage.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react';
+import React, { useState } from 'react';
 
 import Boat from '../../Images/Collage/BoatPexels-photo-3876404.jpeg';
 import Desert from '../../Images/Collage/DesertPexels-photo-4869960.jpeg';
@@ -10,18 +10,50 @@ import Waterfall from '../../Images/Collage/WaterfallPexels-photo-4227875.jpeg';
 import './Collage.css';
 
 const Collage = () => {
+    const [destinations, setDestinations] = useState([
+        {
+            image: Waterfall,
+            type: 'Adventure',
+            text: 'Discover hidden waterfalls deep within the Amazon forest'
+        },
+        {
+            image: Boat,
+            type: 'Luxury',
+            text: 'Travel through the islands of Bali in a private cruise'
+        },
+        {
+            image: SailAway,
+            type: 'Mystery',
+            text: 'Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+        },
+        {
+            image: SoccerField,
+            type: 'Adventure',
+            text: 'Experience Football ontop of the Himilayan Mountains'
+        },
+        {
+            image: Desert,
+            type: 'Thrill',
+            text: 'Ride through the Sahara Desert on a guided camel tour'
+        },
+    ]);
+
+    window.console.log(setDestinations);
     return (
         <>
             <div className="collage">
                 <div className="collage-content">
                 <h1>Check out these destinations</h1>
                     <div className="collage-content-grid">
-                        <img className='row1' src={Waterfall} alt="Photo of a person stading in front of a waterfall" />
-                        <img className='row1' src={Boat} alt="Aerial photo of a boat in the ocean" />
-                        <br /> {/* Cheap fix for 2/3 grid layout */}
-                        <img className='row2' src={SailAway} alt="Sail Boats sailing close to island" />
-                        <img className='row2' src={SoccerField} alt="Aerial photo of a soccer field" />
-                        <img className='row2' src={Desert} alt="Photo of a camalback exchurange" />
+                        {destinations.map((destination) => {
+                            return (
+                                <>
+                                    <img src={destination.image} alt="images of different vacation destinations" />
+                                    <span>{destination.type}</span>
+                                    <h5>{destination.text}</h5>
+                                </>
+                            )
+                        })};
                     </div>
                 </div>
             </div>
